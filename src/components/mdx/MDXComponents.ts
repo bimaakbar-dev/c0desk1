85756474872f571da8339511119384fcfff3b877
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-08-11 15:36:35 +0700

    Update MDXComponents.ts

--- src/components/mdx/MDXComponents.ts
+++ src/components/mdx/MDXComponents.ts
@@ -1,4 +1,5 @@
 // src/components/layout/MDXComponents.ts
+import User from "./User.astro";
 import Blockquote from "./Blockquote.astro";
 import Changelog from "./Changelog.astro";
 import Code from "./Code.astro";
@@ -8,6 +9,7 @@ import LinkCard from "./LinkCard.astro";
 import CardGrid from "./CardGrid.astro";
 
 export const components = {
+  User,
   ChangelogContainer: Changelog,
   ChangelogItem: Changelog,
   Code,
