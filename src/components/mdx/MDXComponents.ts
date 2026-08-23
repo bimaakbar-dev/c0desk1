4f3a5665848b6b971f870b4fd54ab1dd6a337026
Bima Akbar <81931118+bimaakbar-dev@users.noreply.github.com>
2026-08-23 23:24:43 +0700

    Add files via upload

--- src/components/mdx/MDXComponents.ts
+++ src/components/mdx/MDXComponents.ts
@@ -1,11 +1,11 @@
 // src/components/layout/MDXComponents.ts
-import User from "./User.astro";
-import Quote from "./Quote.astro";
-import Code from "./Code.astro";
-import Kbd from "./KBD.astro";
-import Video from "./Video.astro";
 import Card from "./Card.astro";
 import CardGrid from "./CardGrid.astro";
+import Code from "./Code.astro";
+import Kbd from "./KBD.astro";
+import Quote from "./Quote.astro";
+import User from "./User.astro";
+import Video from "./Video.astro";
 
 export const components = {
   User,
